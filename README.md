91bd4a5db08d5b3148f996903762dea6fe562be9
Teofana <teofanaenachioiu@yahoo.com>
2018-12-28 12:54:31 +0200

    update readme.md

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # StudentGradesManagementSystem
 Application manages students' grades. The app can be used by teachers and students as well. 
 
+Requestments:
+* JavaMail API 
+* Java Activation Framework (JAF)
+* Apache Derby
+
+Libraries:
+* activation.jar 
+* mail.jar
+* derby.jar
+* derbyclient.jar
+* derbyrun.jar
+* derbytools.jar
+
 Useful links:
 * https://db.apache.org/derby/integrate/plugin_help/derby_app.html
 * http://faculty.sdmiramar.edu/jcouture/2014sp/cisc190/webct/manual/tutorial-derby-intellij.asp
+* https://www.tutorialspoint.com/java/java_sending_email.htm
+* https://www.mkyong.com/java/javamail-api-sending-email-via-gmail-smtp-example/
+* https://www.youtube.com/watch?v=yvI9HTP54gI
--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -41,31 +41,6 @@ public class Main extends Application {
         StudentService studentService=new StudentService(repoS,repoT,repoN,utilizatorService);
         TemaService temaService=new TemaService(repoT,repoS);
 
-//        noteService.add("1001","1","10","Foarte bine lucrat!",true);
-//        noteService.add("1002","1","10","Mai bine nu",false);
-//        noteService.add("1012","1","10","Mai bine nu",false);
-//        noteService.update("1001","1","9","Remediere nota",true);
-
-//        try {
-//            utilizatorService.createUser();
-//        } catch (PasswordStorage.CannotPerformOperationException e) {
-//            e.printStackTrace();
-//        }
-//
-//        for(Student student:repoS.findAll()){
-//            try {
-//                utilizatorService.createUser(student);
-//            } catch (PasswordStorage.CannotPerformOperationException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//        for(Profesor profesor:repoP.findAll()){
-//            try {
-//                utilizatorService.createUser(profesor);
-//            } catch (PasswordStorage.CannotPerformOperationException e) {
-//                e.printStackTrace();
-//            }
-//        }
 
 
         //launch(args);
